c6d54019b533a713ee649995abdd9077a6dee49c
dlim <7742500+davejlim@users.noreply.github.com>
2019-10-22 16:53:34 -0700

    Updating with calculator refactor

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -6,25 +6,107 @@
 # answer = Kernel.gets()
 # Kernel.puts(answer)
 
-Kernel.puts("Welcome to Calculator!")
-
-Kernel.puts("What's the first number?")
-number1 = Kernel.gets().chomp() # .chomp() method ensures that the newline character is removed from the input string
-
-Kernel.puts("What's the second number?")
-number2 = Kernel.gets().chomp()
-
-Kernel.puts("What operation woud you like to perform? 1) add 2) subtract 3) multiply 4) divide")
-operator = Kernel.gets().chomp() 
-
-if operator == "1" # important to remember that all inputs are strings
-  result = number1.to_i() + number2.to_i()
-elsif operator == "2"
-  result = number1.to_i() - number2.to_i()
-elsif operator == "3"
-  result = number1.to_i() * number2.to_i()
-else
-  result = number1.to_f() / number2.to_f()       
+def prompt(message)
+  Kernel.puts("=> #{message}")
 end
 
-Kernel.puts("The result is #{result}")
+def valid_number?(num)
+  num.to_i() != 0
+end
+
+def operation_to_message(op)
+  case op
+  when '1'
+    'Adding' # note that you don't need to put the return here because the case is the only expresison in the method
+  when '2'
+    'Subtracting'
+  when '3'
+    'Multiplying'
+  when '4'
+    'Dividing'
+  end
+end
+
+prompt("Welcome to Calculator! Enter your name:")
+
+name = ''
+loop do
+  name = Kernel.gets().chomp()
+
+  if name.empty?()
+    prompt("Make sure to use a valid name")
+  else
+    break
+  end
+end
+
+prompt("Hi #{name}!")
+
+loop do # main loop
+  number1 = ''
+  number2 = ''
+  operator = ''
+
+  loop do
+    prompt("What's the first number?")
+    number1 = Kernel.gets().chomp() # .chomp() method ensures that the newline character is removed from the input string
+
+    if valid_number?(number1)
+      break
+    else
+      prompt("Hmm... that doesn't look like a valid number")
+    end
+  end
+
+  loop do
+    prompt("What's the second number?")
+    number2 = Kernel.gets().chomp()
+
+    if valid_number?(number2)
+      break
+    else
+      prompt("Hmm... that doesn't look like a valid number")
+    end
+  end
+
+  operator_prompt = <<-MSG
+    What operation would you like to  perform?
+    1) add
+    2) subtract
+    3) multiply
+    4) divide
+  MSG
+
+  prompt(operator_prompt)
+  
+  loop do
+    operator = Kernel.gets().chomp()
+  
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt("Must choose 1, 2, 3, or 4")
+    end
+  end
+
+  prompt("#{operation_to_message(operator)} the two numbers...")
+
+  result = case operator
+           when '1'
+             number1.to_i() + number2.to_i()
+           when '2'
+             number1.to_i() - number2.to_i()
+           when '3'
+             number1.to_i() * number2.to_i()
+           when '4'
+             number1.to_f() / number2.to_f()
+  end
+
+  prompt("The result is #{result}")
+
+  prompt("Do you want to perofrm another calculation? (Y to calculate again)")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt ("Thank you for using the calculator. Goodbye!")
---
+++ lesson_2/pseudocode.rb
@@ -0,0 +1,31 @@
+pseudocode.rb 
+
+# For example, write out pseudo-code (both casual and formal) that does the following:
+
+1. a method that returns the sum of two integers
+
+Given two integers
+Sum two integers
+Print
+
+START
+Given two integers
+SET integer1
+SET integer2
+SET SUM = integer1 + integer2
+Print SUM
+
+2. method that takes an array of strings, and returns a string that is all those strings concatenated together
+
+Given array of strings
+Concatenate strings
+Return concatenated strings
+
+3. method that takes an array of integers, and returns a new array with every other element
+
+Given array of integers
+Iterate through the array one by one
+  - Assign the first value to the new array
+  - Skip the next value
+  - Assign the next value to the new array
+  - Do the above until the original array of integers has been assigned or skipped
