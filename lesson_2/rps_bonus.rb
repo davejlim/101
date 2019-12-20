fdb8c5ef3408ecb650920a4e7465fedbd9f2e92a
dlim <7742500+davejlim@users.noreply.github.com>
2019-12-19 20:37:36 -0800

    Worked on additional features for rps_bonus.rb

--- lesson_2/rps_bonus.rb
+++ lesson_2/rps_bonus.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
 
 MOVE_BEATS = { rock: ['scissors', 'lizard'],
@@ -14,26 +16,46 @@ def win?(first, second)
   MOVE_BEATS[first].include?(second)
 end
 
-def display_results(player, computer)
+def display_results(player, computer, player_win, computer_win)
   if win?(player.to_sym, computer)
     prompt("You won!")
+    player_win += 1
   elsif win?(computer.to_sym, player)
     prompt("Computer won!")
+    computer_win += 1
   else
     prompt("It's a tie")
   end
 end
 
+def score
+  if win(player.to_sym, computer)
+    player_win += 1
+  elsif win?(computer.to_sym, player)
+    computer_win += 1
+  end
+  prompt()
+
+def clear
+  system('clear') || system('cls')
+end
+
+player_score = 0
+computer_score = 0
+
 loop do
+  clear
   choice = ''
+  prompt("Welcome to Rock Paper Scissors Spock Lizard!")
+
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    prompt("To play, choose one: #{VALID_CHOICES.join(', ')}")
     choice = Kernel.gets().chomp()
 
     if VALID_CHOICES.include?(choice)
       break
     else
-      prompt("That's not a valid choice.")
+      prompt("Sorry, but that's not a valid choice.")
     end
   end
 
@@ -41,11 +63,14 @@ loop do
 
   Kernel.puts("You chose: #{choice}; Computer chose: #{computer_choice}")
 
-  display_results(choice, computer_choice)
+  display_results(choice, computer_choice, player_score, computer_score)
+
+  prompt("Your Score: #{player_score}. Computer's Score: #{computer_score}.")
+  binding.pry
 
   prompt("Do you want to play again?")
   answer = Kernel.gets().chomp()
   break unless answer.downcase().start_with?('y')
 end
 
-prompt("Thank you for playing. Goodbye!")
+prompt("Thanks for playing. Goodbye!")
