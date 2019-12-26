45c557088f206b4e020567e586cd33869d88a28d
davejlim <admin@dave.lan>
2019-12-26 15:06:36 -0500

    Implementing feature 3 for RPS Bonus to determine a winner after 5 matches.

--- lesson_2/rps_bonus.rb
+++ lesson_2/rps_bonus.rb
@@ -16,57 +16,69 @@ def win?(first, second)
   MOVE_BEATS[first].include?(second)
 end
 
-def display_results(player, computer, player_win, computer_win)
+def display_results(player, computer)
   if win?(player.to_sym, computer)
-    prompt("You won!")
-    player_win += 1
+    prompt("You won this match!")
   elsif win?(computer.to_sym, player)
-    prompt("Computer won!")
-    computer_win += 1
+    prompt("Computer won this match!")
   else
     prompt("It's a tie")
   end
 end
 
-def score
-  if win(player.to_sym, computer)
-    player_win += 1
+def score(player, computer, which_score)
+  if win?(player.to_sym, computer)
+    which_score[0] += 1
   elsif win?(computer.to_sym, player)
-    computer_win += 1
+    which_score[1] += 1
   end
-  prompt()
+end
+
+def win_condition?(win_count)
+  win_count.include?(5)
+end
 
 def clear
   system('clear') || system('cls')
 end
 
-player_score = 0
-computer_score = 0
-
 loop do
   clear
   choice = ''
-  prompt("Welcome to Rock Paper Scissors Spock Lizard!")
-
+  player_computer_score = [0, 0] #player score is first and computer score is second in array
+  prompt("Welcome to Rock Paper Scissors Spock Lizard! The grand winner is the first person to win 5 matches.")
+  
   loop do
-    prompt("To play, choose one: #{VALID_CHOICES.join(', ')}")
-    choice = Kernel.gets().chomp()
 
-    if VALID_CHOICES.include?(choice)
-      break
-    else
-      prompt("Sorry, but that's not a valid choice.")
+    loop do
+      prompt("To play, choose one: #{VALID_CHOICES.join(', ')}")
+      choice = Kernel.gets().chomp()
+
+      if VALID_CHOICES.include?(choice)
+        break
+      else
+        prompt("Sorry, but that's not a valid choice.")
+      end
     end
+
+    computer_choice = VALID_CHOICES.sample
+
+    Kernel.puts("You chose: #{choice}; Computer chose: #{computer_choice}")
+
+    display_results(choice, computer_choice)
+
+    score(choice, computer_choice, player_computer_score)
+
+    prompt("Your Score: #{player_computer_score[0]}. Computer's Score: #{player_computer_score[1]}.")
+
+    break if win_condition?(player_computer_score)
   end
 
-  computer_choice = VALID_CHOICES.sample
-
-  Kernel.puts("You chose: #{choice}; Computer chose: #{computer_choice}")
-
-  display_results(choice, computer_choice, player_score, computer_score)
-
-  prompt("Your Score: #{player_score}. Computer's Score: #{computer_score}.")
-  binding.pry
+  if player_computer_score[0] == 5
+    prompt("Congratulations on being the grand winner of the game!")
+  else
+    prompt("Defeat! Please try again.")
+  end
 
   prompt("Do you want to play again?")
   answer = Kernel.gets().chomp()
