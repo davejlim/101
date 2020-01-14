912cc3fef79fc2ac4168c73d0c90d3552d8f53b9
dlim <7742500+davejlim@users.noreply.github.com>
2020-01-13 16:07:12 -0800

    Finished last refactor of loan_calculator.rb

--- lesson_2/loan_calculator.rb
+++ lesson_2/loan_calculator.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
@@ -10,13 +12,43 @@ def valid_number(input)
   input.to_i > 0 && input.to_i.to_s == input || input.to_f.to_s == input
 end
 
+def retrieve_amount
+  loop do
+    prompt('To start, please share the total amount of your loan.')
+    loan_amount = gets.chomp
+    prompt(loan_amount)
+    return loan_amount if valid_number(loan_amount)
+    invalid_number
+  end
+end
+
+def retrieve_apr
+  loop do
+    prompt('Next enter your annual percentage rate (APR) without a % symbol.')
+    apr = gets.chomp
+    prompt(apr)
+    return apr if valid_number(apr)
+    invalid_number
+  end
+end
+
+def retrieve_duration
+  loop do
+    prompt('Last step! Just enter your loan duration in months:')
+    loan_duration_months = gets.chomp
+    prompt(loan_duration_months)
+    return loan_duration_months if valid_number(loan_duration_months)
+    invalid_number
+  end
+end
+
 def monthly_payment_calculation(loan, interest, duration)
-  monthly_payment = loan * (interest / (1 - (1 + interest)**(-duration)))
+  loan * (interest / (1 - (1 + interest)**-duration))
 end
 
 def another_calculation?
-  prompt("Would you like to calculate the monthly payment of another loan? Y/N")
-  gets.chomp.downcase.start_with?("y")
+  prompt('Would you like to calculate the monthly payment of another loan? Y/N')
+  gets.chomp.downcase.start_with?('y')
 end
 
 def clear
@@ -24,65 +56,15 @@ def clear
 end
 
 loop do
-  loan_amount = ''
-  apr = ''
-  loan_duration_months = ''
-
   clear
-
-  prompt("Welcome to Loan Calculator! We can calculate your monthly payment.")
-
-  loop do
-    prompt("To start, please share the total amount of your loan.")
-    loan_amount = gets.chomp
-
-    prompt(loan_amount)
-
-    if valid_number(loan_amount)
-      break
-    else
-      invalid_number
-    end
-  end
-
-  loop do
-    prompt("Next enter your annual percentage rate (APR) without a % symbol.")
-
-    apr = gets.chomp
-
-    if valid_number(apr)
-      break
-    else
-      invalid_number
-    end
-  end
-
-  prompt(apr)
-
-  loop do
-    prompt("Last step! Just enter your loan duration in months:")
-
-    loan_duration_months = gets.chomp
-
-    if valid_number(loan_duration_months)
-      break
-    else
-      invalid_number
-    end
-  end
-
-  prompt(loan_duration_months)
-
+  prompt('Welcome to Loan Calculator! We can calculate your monthly payment.')
+  loan_amount = retrieve_amount
+  apr = retrieve_apr
+  loan_duration_months = retrieve_duration
   monthly_interest_rate = apr.to_f / 1200
-
-  prompt(monthly_interest_rate)
-
-  # monthly_payment_calculation(loan_amount.to_f, monthly_interest_rate.to_f, loan_duration_months.to_f)
-  monthly_payment = loan_amount.to_f * (monthly_interest_rate.to_f / (1 - (1 + monthly_interest_rate.to_f)**(-loan_duration_months.to_f)))
-
+  monthly_payment = monthly_payment_calculation(loan_amount.to_f, monthly_interest_rate.to_f, loan_duration_months.to_f)
   prompt("Your monthly payment is $#{format('%02.2f', monthly_payment)}.")
-
   break unless another_calculation?
 end
 
-prompt("Thank you for using our calculator! Goodbye!")
+prompt('Thank you for using our calculator! Goodbye!')
