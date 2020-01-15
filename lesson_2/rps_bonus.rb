e24e0b59e60e7b33a51cf68592bec6e312ade180
dlim <7742500+davejlim@users.noreply.github.com>
2020-01-14 19:50:56 -0800

    Refactor taking into consideration Srdjan's code reivew

--- lesson_2/rps_bonus.rb
+++ lesson_2/rps_bonus.rb
@@ -12,6 +12,13 @@ MOVE_BEATS = { rock: %w(scissors lizard),
                spock: %w(scissors rock),
                lizard: %w(spock paper) }.freeze
 
+END_ABBREVS = { yes: 'y',
+                no: 'n' }.freeze
+
+END_OPTIONS = END_ABBREVS.flatten.map(&:to_s).freeze
+
+WIN_NUMBER = 5
+
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
@@ -33,7 +40,7 @@ def answer
   loop do
     prompt("To play, choose one: #{ABBREVS.keys.join(', ')}")
     prompt("Or you can use an abbreviation: #{ABBREVS.values.join(', ')}")
-    choice = Kernel.gets.chomp
+    choice = Kernel.gets.chomp.downcase
 
     break if VALID_CHOICES.include?(choice)
     prompt("Sorry, but that's not a valid choice.")
@@ -51,16 +58,30 @@ def display_results(player, computer)
   end
 end
 
-def score(player, computer, which_score)
+def increment_score(player, computer, score)
   if win?(player.to_sym, computer)
-    which_score[0] += 1
+    score[0] += 1
   elsif win?(computer.to_sym, player)
-    which_score[1] += 1
+    score[1] += 1
   end
 end
 
 def win_condition?(win_count)
-  win_count.include?(5)
+  win_count.include?(WIN_NUMBER)
+end
+
+def play_again?
+  loop do
+    prompt("Do you want to play again? Choose: #{END_ABBREVS.keys.join(', ')}")
+    prompt("Or you can also enter: #{END_ABBREVS.values.join(', ')}")
+    response = Kernel.gets.chomp.downcase
+    return response if END_OPTIONS.include?(response)
+    prompt("Sorry, but that's not a valid choice.")
+  end
+end
+
+def end_game?(end_game_response)
+  end_game_response == 'no' || end_game_response == 'n'
 end
 
 def clear
@@ -72,7 +93,7 @@ loop do
   choice = ''
   player_computer_score = [0, 0] # player score 1st; computer score 2nd
   prompt('Welcome to Rock Paper Scissors Spock Lizard!')
-  prompt('The grand winner is the first person to win 5 matches.')
+  prompt("The grand winner is the first person to win #{WIN_NUMBER} matches.")
 
   loop do
     choice = answer
@@ -83,7 +104,7 @@ loop do
 
     display_results(choice, computer_choice)
 
-    score(choice, computer_choice, player_computer_score)
+    increment_score(choice, computer_choice, player_computer_score)
 
     prompt("Your Score: #{player_computer_score[0]}.")
     prompt("Computer's Score: #{player_computer_score[1]}.")
@@ -91,15 +112,15 @@ loop do
     break if win_condition?(player_computer_score)
   end
 
-  if player_computer_score[0] == 5
+  if player_computer_score[0] == WIN_NUMBER
     prompt('Congratulations on being the grand winner of the game!')
   else
     prompt('Defeat! The computer is the grand winner of the game.')
   end
 
-  prompt('Do you want to play again?')
-  answer = Kernel.gets.chomp
-  break unless answer.downcase.start_with?('y')
+  play_again_response = play_again?
+
+  break if end_game?(play_again_response)
 end
 
 prompt('Thanks for playing. Goodbye!')
