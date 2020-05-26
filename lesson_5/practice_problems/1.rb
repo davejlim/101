e2cf43106875fc96c89202e034a4795d67f11ef6
dlim <7742500+davejlim@users.noreply.github.com>
2020-05-26 16:24:27 -0700

    Uploading Lesson 5 dir and beginning exercises

---
+++ lesson_5/practice_problems/1.rb
@@ -0,0 +1,18 @@
+#  RB101 - Lesson 5 - Practice Problems: Sorting, Nested Collections and Working with Blocks
+
+# How would you order this array of number strings by descending numeric value?
+
+arr = ['10', '11', '9', '7', '8']
+
+int_arr = arr.map {|str| str.to_i}
+
+p int_arr.sort { |a, b| b <=> a }
+
+# Could have also made this condensed by 
+
+arr.sort do |a, b|
+  b.to_i <=> a.to_i
+end
+# => ["11", "10", "9", "8", "7"]
+
+# Since strings are compared character by character, '11' would be evaluated to be lesser than '7'. We would need to compare them numerically by converting them to integers.
