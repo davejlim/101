3b83f851bac01b9178644dd57b904fa7e2f46e20
dlim <7742500+davejlim@users.noreply.github.com>
2020-05-28 16:36:06 -0700

    Finished Lesson 5

---
+++ lesson_5/working_blocks.rb
@@ -0,0 +1,7 @@
+# RB101 - Lesson 5 - Advanced Ruby Collections - Working with Blocks
+
+p [{ a: 'ant', b: 'elephant' }, { c: 'cat' }].select do |hash|
+  hash.all? do |key, value|
+    value[0] == key.to_s
+  end
+end
