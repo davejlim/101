49b2ac8adbb5990adc01ab0b6b638c0f7231c564
dlim <7742500+davejlim@users.noreply.github.com>
2020-05-31 17:13:59 -0700

    First Walkthrough of Tic Tac Toe

--- lesson_6/tictactoe/tictactoe.rb
+++ lesson_6/tictactoe/tictactoe.rb
@@ -11,6 +11,7 @@ end
 
 def display_board(brd)
   system 'clear'
+  puts "You're #{PLAYER_MARKER}'s'. Computer's is #{COMPUTER_MARKER}'s."
   puts ""
   puts "  1  |  2  |  3"
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
@@ -59,19 +60,53 @@ def board_full?(brd)
   empty_squares(brd).empty?
 end
 
-def someone_won?(brd)
-  false
+def detect_winner(brd)
+  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
+                  [[1, 5, 9], [3, 5, 7]]               # diagnols
+
+  winning_lines.each do |line|
+    if brd[line[0]] == PLAYER_MARKER &&
+       brd[line[1]] == PLAYER_MARKER &&
+       brd[line[2]] == PLAYER_MARKER
+      return 'Player'
+    elsif brd[line[0]] == COMPUTER_MARKER &&
+          brd[line[1]] == COMPUTER_MARKER &&
+          brd[line[2]] == COMPUTER_MARKER
+      return 'Computer'
+    end
+  end
+  nil
 end
 
-board = initialize_board
-display_board(board)
+def someone_won?(brd)
+  !!detect_winner(brd)
+end
 
-loop do
-  player_places_piece!(board)
-  computer_places_piece!(board)
+loop do 
+  board = initialize_board
+
+  loop do
+    display_board(board)
+
+    player_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+
+    computer_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+  end
+
   display_board(board)
 
-  break if someone_won?(board) || board_full?(board)
+  if someone_won?(board)
+    prompt(" #{detect_winner(board)} won!")
+  else
+    prompt "It's a tie!"
+  end
+
+  prompt("Would you like to play again? (y or n)")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
 end
 
-display_board(board)
+prompt "Thanks for playing Tic Tac Toe! Goodbye :)"
