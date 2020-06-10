1bd4455de24c3f320d4238183442fe547d575d60
dlim <7742500+davejlim@users.noreply.github.com>
2020-06-10 14:39:47 -0700

    Updated tictactoe.rb

--- lesson_6/tictactoe/tictactoe.rb
+++ lesson_6/tictactoe/tictactoe.rb
@@ -9,6 +9,10 @@ INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
 WINNING_SCORE = 5
+FIRST_MOVE = 'choose'
+VALID_MOVE_CHOICES = ['player', 'computer']
+
+# rubocop:disable Metrics/LineLength
 
 def prompt(msg)
   puts "=> #{msg}"
@@ -17,15 +21,15 @@ end
 def joinor(array, separator=', ', word='or')
   n = array.size
   if n < 2
-    "#{array[0]}"
+    array[0].to_s
   elsif n == 2
     array.join(" #{word} ")
   else
-    "#{array.slice(0, n - 1).join("#{separator}")}#{separator}#{word} #{array[n-1]}"
+    "#{array.slice(0, n - 1).join(separator.to_s)}#{separator}#{word} #{array[n - 1]}"
   end
 end
 
-# rubocop:disable Metrics/AbcSize
+# rubocop:disable Metrics/AbcSize, MethodLength
 def display_board(brd, p_scr, c_scr, game_num)
   system 'clear'
   puts "You're #{PLAYER_MARKER}'s'. Computer's is #{COMPUTER_MARKER}'s."
@@ -45,7 +49,7 @@ def display_board(brd, p_scr, c_scr, game_num)
   puts "     |     |"
   puts ""
 end
-# rubocop:enable Metrics/AbcSize
+# rubocop:enable Metrics/AbcSize, MethodLength
 
 def initialize_board
   new_board = {}
@@ -80,8 +84,28 @@ def computer_places_piece!(brd)
   elsif immediate_threat?(brd) == true
     computer_logic(brd, PLAYER_MARKER)
   # random
+  elsif brd[5] == INITIAL_MARKER
+    brd[5] = COMPUTER_MARKER
   else
     brd[square] = COMPUTER_MARKER
+  end
+end
+
+def place_piece!(brd, who)
+  case who
+  when 'player'
+    player_places_piece!(brd)
+  when 'computer'
+    computer_places_piece!(brd)
+  end
+end
+
+def alternate_player(who)
+  case who
+  when 'player'
+    'computer'
+  when 'computer'
+    'player'
   end
 end
 
@@ -124,7 +148,7 @@ def computer_logic(brd, marker)
   WINNING_LINES.each do |line|
     if brd.values_at(*line).count(marker) == 2 && brd.values_at(*line).count(INITIAL_MARKER) == 1
       brd[line[brd.values_at(*line).index(INITIAL_MARKER)]] = COMPUTER_MARKER
-      return
+      break
     end
   end
 end
@@ -132,17 +156,33 @@ end
 player_score = 0
 computer_score = 0
 game_number = 1
+first_move = ''
 
 loop do
   board = initialize_board
 
+  if FIRST_MOVE == 'choose'
+    loop do
+      system 'clear'
+      prompt("Choose who goes first: #{joinor(VALID_MOVE_CHOICES)}")
+      first_move = gets.chomp.downcase
+      break if VALID_MOVE_CHOICES.include?(first_move)
+      prompt("Invalid choice!")
+    end
+  end
+
+  case first_move
+  when 'player'
+    current_player = 'player'
+  when 'computer'
+    current_player = 'computer'
+  end
+
   loop do
     display_board(board, player_score, computer_score, game_number)
 
-    player_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
-
-    computer_places_piece!(board)
+    place_piece!(board, current_player)
+    current_player = alternate_player(current_player)
     break if someone_won?(board) || board_full?(board)
   end
 
@@ -155,12 +195,12 @@ loop do
       player_score += 1
     when 'Computer'
       computer_score += 1
-    end    
-    prompt("The total score is - Player: #{player_score}; Computer: #{computer_score}")
+    end
   else
     prompt "It's a tie!"
-    prompt("The total score is - Player: #{player_score}; Computer: #{computer_score}")
   end
+
+  prompt("The total score is - Player: #{player_score}; Computer: #{computer_score}")
 
   if game_over?(player_score, computer_score) == true
     break
@@ -182,8 +222,3 @@ elsif computer_score == WINNING_SCORE
 else
   prompt("Thanks for playing Tic Tac Toe! Goodbye :)")
 end
-
-# prompt("Would you like to play again?")
-# answer = gets.chomp
-# break unless answer.downcase.start_with?('y')
-
