b7aa45af32099af4f68be73a3169935af871ad3f
dlim <7742500+davejlim@users.noreply.github.com>
2020-06-22 17:27:22 -0700

    Finished Multiplying Two Numbers

---
+++ rb101_exercises/easy_3/multiplying.rb
@@ -0,0 +1,10 @@
+# RB101-109 Small Problems - Easy 3 - Multiplying Two Numbers
+
+# Create a method that takes two arguments, multiplies them together,
+# and returns the result.
+
+def multiply(first, second)
+  first * second
+end
+
+p multiply([1, 2, 3], 3)
