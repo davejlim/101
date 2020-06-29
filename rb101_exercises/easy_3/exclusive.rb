22f57df047db172588c89f1d7da1eede4ead5f5a
davejlim <admin@dave.local>
2020-06-29 11:27:25 -0400

    Finished Exclusive Or exercise.

---
+++ rb101_exercises/easy_3/exclusive.rb
@@ -0,0 +1,36 @@
+# RB101-109 Small Problems - Easy 3 - Exclusive Or
+#
+# The || operator returns a truthy value if either or both of its operands
+# are truthy, a falsey value if both operands are falsey. The && operator
+# returns a truthy value if both of its operands are truthy, and a falsey
+# value if either operand is falsey. This works great until you need only
+# one of two conditions to be truthy, the so-called exclusive or.
+
+# In this exercise, you will write a method named xor that takes two arguments,
+# and returns true if exactly one of its arguments is truthy, false otherwise.
+
+# Input: Evaluated expression
+# Output: Boolean
+# Data Structure: nil
+#
+# Pass in two arguments
+# If first evaluates to true and second evaluates to false, return true
+# If first evaluates to false and second evaluates to true, return true
+# Else, return false
+
+def xor(first, second)
+  if first == true && second == false
+    return true
+  elsif first == false && second == true
+    return true
+  else
+    return false
+  end
+end
+
+puts xor(true, false) == true
+puts xor(false, true) == true
+puts xor(true, true) == false
+puts xor(false, false) == false
+
+
