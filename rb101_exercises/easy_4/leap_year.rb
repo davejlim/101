f2a7e135d0a110a33b92a0dddb4923312aa57de3
davejlim <admin@dave.lan>
2020-07-01 17:14:26 -0400

    Finished both parts of Leap Year exercise.

--- rb101_exercises/easy_4/leap_year.rb
+++ rb101_exercises/easy_4/leap_year.rb
@@ -20,8 +20,18 @@
   # Elsif return true
 # Else return false
 
+
+# Part 2 - The British Empire adopted the Gregorian Calendar in 1752, which was a
+# leap year. Prior to 1752, the Julian Calendar was used. Under the Julian
+# Calendar, leap years occur in any year that is evenly divisible by 4.
+
+# Using this information, update the method from the previous exercise to
+# determine leap years both before and after 1752.
+
 def leap_year?(year)
-  if year % 4 == 0
+  if julian?(year)
+    year % 4 == 0
+  elsif year % 4 == 0
     if year % 100 == 0
       return true if year % 400 == 0
       false
@@ -33,6 +43,23 @@ def leap_year?(year)
   end
 end
 
+def julian?(year)
+  year < 1752
+end
+
+# puts leap_year?(2016) == true
+# puts leap_year?(2015) == false
+# puts leap_year?(2100) == false
+# puts leap_year?(2400) == true
+# puts leap_year?(240000) == true
+# puts leap_year?(240001) == false
+# puts leap_year?(2000) == true
+# puts leap_year?(1900) == false
+# puts leap_year?(1752) == true
+# puts leap_year?(1700) == false
+# puts leap_year?(1) == false
+# puts leap_year?(100) == false
+# puts leap_year?(400) == true
 
 puts leap_year?(2016) == true
 puts leap_year?(2015) == false
@@ -43,7 +70,7 @@ puts leap_year?(240001) == false
 puts leap_year?(2000) == true
 puts leap_year?(1900) == false
 puts leap_year?(1752) == true
-puts leap_year?(1700) == false
+puts leap_year?(1700) == true
 puts leap_year?(1) == false
-puts leap_year?(100) == false
+puts leap_year?(100) == true
 puts leap_year?(400) == true
