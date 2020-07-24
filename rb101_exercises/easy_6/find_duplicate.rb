88f147bf03c8d72e5ea6c76e3c8c3506c68565d1
dlim <7742500+davejlim@users.noreply.github.com>
2020-07-23 17:49:59 -0700

    Finished Find Duplicates Exercise

--- rb101_exercises/easy_6/find_duplicate.rb
+++ rb101_exercises/easy_6/find_duplicate.rb
@@ -21,18 +21,54 @@
 
 require 'pry'
 
+#def find_dup(array)
+#  potential_dup = []
+#
+#  array.each_with_object(potential_dup) do | num, potential_dup |
+#    potential_dup << num  if (potential_dup.last == num || potential_dup.empty?)
+#  end
+#
+#  potential_dup.last
+#end
+
 def find_dup(array)
-  potential_dup = []
-
-  array.each_with_object(potential_dup) do | num, potential_dup |
-    potential_dup << num  if (potential_dup.last == num || potential_dup.empty?)
+  duplicate_array = []
+  array.each do |num|
+    return num if duplicate_array.include?(num)
+    duplicate_array << num
   end
+end
 
-  potential_dup.last
+def find_dup2(array)
+  array.find { |num| array.count(num) == 2 }
 end
 
 p find_dup([1, 5, 3, 1])
 p find_dup([18,  9, 36, 96, 31, 19, 54, 75, 42, 15,
+                    38, 25, 97, 92, 46, 69, 91, 59, 53, 27,
+                              14, 61, 90, 81,  8, 63, 95, 99, 30, 65,
+                                        78, 76, 48, 16, 93, 77, 52, 49, 37, 29,
+                                                  89, 10, 84,  1, 47, 68, 12, 33, 86, 60,
+                                                            41, 44, 83, 35, 94, 73, 98,  3, 64, 82,
+                                                                      55, 79, 80, 21, 39, 72, 13, 50,  6, 70,
+                                                                                85, 87, 51, 17, 66, 20, 28, 26,  2, 22,
+                                                                                          40, 23, 71, 62, 73, 32, 43, 24,  4, 56,
+                                                                                                    7,  34, 57, 74, 45, 11, 88, 67,  5, 58])
+
+p find_dup2([1, 5, 3, 1]) == 1
+p find_dup2([18,  9, 36, 96, 31, 19, 54, 75, 42, 15,
+                    38, 25, 97, 92, 46, 69, 91, 59, 53, 27,
+                              14, 61, 90, 81,  8, 63, 95, 99, 30, 65,
+                                        78, 76, 48, 16, 93, 77, 52, 49, 37, 29,
+                                                  89, 10, 84,  1, 47, 68, 12, 33, 86, 60,
+                                                            41, 44, 83, 35, 94, 73, 98,  3, 64, 82,
+                                                                      55, 79, 80, 21, 39, 72, 13, 50,  6, 70,
+                                                                                85, 87, 51, 17, 66, 20, 28, 26,  2, 22,
+                                                                                          40, 23, 71, 62, 73, 32, 43, 24,  4, 56,
+                                                                                                    7,  34, 57, 74, 45, 11, 88, 67,  5, 58]) == 73
+
+p find_dup2([1, 5, 3, 1])
+p find_dup2([18,  9, 36, 96, 31, 19, 54, 75, 42, 15,
                     38, 25, 97, 92, 46, 69, 91, 59, 53, 27,
                               14, 61, 90, 81,  8, 63, 95, 99, 30, 65,
                                         78, 76, 48, 16, 93, 77, 52, 49, 37, 29,
