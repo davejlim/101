20180e34541b7a0908179e101d90d7e2fc073e4c
dlim <7742500+davejlim@users.noreply.github.com>
2020-08-12 16:40:29 -0700

    Finished Welcome Stranger exercise

---
+++ rb101_exercises/easy_9/welcome_stranger.rb
@@ -0,0 +1,15 @@
+# RB101-109 - Small Problems - Easy 9 - Welcome Stranger
+#
+# Create a method that takes 2 arguments, an array and a hash. The array will
+# contain 2 or more elements that, when combined with adjoining spaces, will
+# produce a person's name. The hash will contain two keys, :title and
+# :occupation, and the appropriate values. Your method should return a
+# greeting that uses the person's full name, and mentions the person's title
+# and occupation.
+
+
+def greetings(ary, hash)
+  puts "Hello, #{ary.join(' ')}! Nice to have a #{hash[:title]} #{hash[:occupation]} around."
+end
+
+greetings(['John', 'Q', 'Doe'], { title: 'Master', occupation: 'Plumber' })
